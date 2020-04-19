4d4edf0edbf48948a7a8dd2175b4fdc0e7e8b363
Nykollas <nicolauarti09@gmail.com>
2020-04-19 19:52:21 -0300

    Procfile definido

---
+++ build/precache-manifest.7cc844d313e74a69b31f8b445dda0c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc57608e3448d0ca09bbc5d27678a304",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f25940f5369ff7e34d5",
+    "url": "/static/css/main.69279404.chunk.css"
+  },
+  {
+    "revision": "62d00132ce3b738eb1a2",
+    "url": "/static/js/2.771e3fdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.771e3fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f25940f5369ff7e34d5",
+    "url": "/static/js/main.e984d19d.chunk.js"
+  },
+  {
+    "revision": "a9dc5e0f9e1581dd1eb8",
+    "url": "/static/js/runtime-main.760107b5.js"
+  }
+]);
